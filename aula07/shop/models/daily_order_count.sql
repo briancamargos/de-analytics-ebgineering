731ed0bab40472e62ab727a48353be6eeb103371
Brian Camargos <brian.camargos@gmail.com>
2023-03-29 21:48:50 -0300

    omg

--- aula07/shop/models/daily_order_count.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-   date(shipping_limit_date) as shipping_date, 
-   count(order_id) as num_orders 
-FROM 
-   `shiba_ecommerce.order_items` 
-where 
-   date(shipping_limit_date)='{{ var("ingestion_date") }}' 
-group by 1
--- aula07/shop/models/orders_by_year.sql
+++
@@ -1,6 +0,0 @@
-{{ config(materialized='table') }}
-select 
-  extract(year from date(order_approved_at)) as year, 
-  count(order_id) as total
-  from `dbt_ecommerce.orders` 
-  group by 1
